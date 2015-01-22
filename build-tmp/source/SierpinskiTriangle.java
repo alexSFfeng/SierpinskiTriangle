26783c9e588cc3aef188d21ff2c1391bde63a617
unknown <shfeng1@697-334-Shelob.sf.k12.internal>
2015-01-22 10:33:08 -0800

    submitting assignment

--- build-tmp/source/SierpinskiTriangle.java
+++ build-tmp/source/SierpinskiTriangle.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SierpinskiTriangle extends PApplet {
+
 int limit = 10;
 int r = 125;
 int g = 249; 
@@ -38,5 +54,14 @@ public void sierpinski(int x, int y, int len)
   else
   {
    triangle(x,y,x+len,y,x+len/2,y-len);
+  }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SierpinskiTriangle" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
